d45615215b255c9bf2373308789717557cf1c0f7
msc24h18 Paul Uriarte Vicandi (puriarte) <msc24h18@steghorn.ee.ethz.ch>
2024-09-18 11:24:11 +0200

    Update plot_xyce

--- utils/src/plot_xyce.py
+++ utils/src/plot_xyce.py
@@ -7,21 +7,38 @@ import argparse
 plt.style.use("ggplot")
 
 # Function to read the file and plot data
-def plot_data(filename):
-    # Read the file using pandas
-    data = pd.read_csv(filename, delim_whitespace=True, skipfooter=1)  # Assuming space/tab delimited
-    print(data)
-    # Set the second column as TIME
-    time = data.iloc[:, 1]
+def plot_data(filename, xaxis, yaxis):
+    """
+    Plot the data read from a text file outputted by Xyce.
 
-    # Plot all other columns against TIME
-    for column in data.columns[2:]:
-        plt.plot(time, data[column], label=column)
+    Args:
+        filename: (str) The name of the file to read.
+        xaxis: (str) The name of the column to be used as the x-axis. If None, 'TIME' will be used.
+        yaxis: (list(str)) A list of strings with the names of the columns to plot against xaxis. If None, all other columns will be used.
+
+    Returns:
+        None
+    """
+
+    # Read the file using pandas. Skip the last line (contains ending message).
+    data = pd.read_csv(filename, delim_whitespace=True, skipfooter=1, engine='python')
+    # drop 'Index' column
+    data = data.drop(['Index'], axis=1)
+    print(data)
+
+    # select the column to be used as the X-axis ('TIME' by default)
+    xaxis = 'TIME' if xaxis is None else xaxis
+    x = data[xaxis]
+
+    # Plot columns in yaxis against time.
+    for column in data.columns:
+        if (column != xaxis) and ((yaxis is None) or (column in yaxis)):
+            plt.plot(x, data[column], label=column)
 
     # Adding labels and title
-    plt.xlabel('TIME')
+    plt.xlabel(xaxis)
     plt.ylabel('Values')
-    plt.title('Data Plot')
+    plt.title('Xyce Data Plot')
     plt.legend()
     plt.grid(True)
 
@@ -35,12 +52,13 @@ def main():
 
     # Add argument for the filename
     parser.add_argument('filename', type=str, help='The path to the text file')
+    parser.add_argument('-x', type=str, help='The name of the column to use as the X-axis')
+    parser.add_argument('-y', type=str, help='The names of the columns to use as the Y-axis')
 
     # Parse the arguments
     args = parser.parse_args()
-
     # Call the plot function with the filename
-    plot_data(args.filename)
+    plot_data(args.filename, args.x, None if args.y is None else args.y.split(" "))
 
 # Entry point of the script
 if __name__ == "__main__":
