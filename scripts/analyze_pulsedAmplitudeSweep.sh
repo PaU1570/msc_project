f16249bee1bee49f82674b446457844330482a72
msc24h18 Paul Uriarte Vicandi (puriarte) <msc24h18@badile09.ee.ethz.ch>
2024-09-25 11:27:02 +0200

    Update script

--- scripts/analyze_pulsedAmplitudeSweep.sh
+++ scripts/analyze_pulsedAmplitudeSweep.sh
@@ -4,19 +4,39 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 PARENT_DIR=$(dirname "$SCRIPT_DIR")
 ANALYSIS_SCRIPT_SOURCE="${PARENT_DIR}/MLP_NeuroSim_V3.0/nonlinear_fit.py"
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <directory>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <directory> <output_directory>"
     exit 1
 fi
 
 directory="$1"
+output_directory="$2"
+# Create the output directory if it doesn't exist
+if [ ! -d "$output_directory" ]; then
+    mkdir -p "$output_directory"
+fi
+
+error_files="${output_directory}/error_files.txt"
+error_log="${output_directory}/error_log.log"
+
 echo "Analyzing data in ${directory}"
+
 
 # Find all csv files in the directory that contain the string "pulsedAmplitudeSweep" and don't contain the string "(ALL)"
 files=$(find ${directory} -type f -name "*pulsedAmplitudeSweep*.csv" | grep -v "(ALL)")
 for file in $files; do
-    echo "$file"
-    #python ${ANALYSIS_SCRIPT_SOURCE} $file saveplot > /dev/null
+    echo -e "\e[33m$file\e[0m"
+    file_output_dir="${output_directory}/$(dirname $file)"
+    if [ ! -d "$file_output_dir" ]; then
+        mkdir -p "$file_output_dir"
+    fi
+
+    if ! python ${ANALYSIS_SCRIPT_SOURCE} $file --plotmode saveplot --config --summary --dest ${file_output_dir} 2>> ${error_log} ; then
+        echo -e "\e[31mError\e[0m analyzing file (see ${error_files} and ${error_log})"
+        echo "$file" >> ${error_files}
+    else
+        echo -e "\e[32mDone\e[0m"
+    fi
 done
 
 echo "Done."
