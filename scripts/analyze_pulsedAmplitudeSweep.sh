28a6fdeccb02a537ec1b0d314844bf12445ba9e2
msc24h18 Paul Uriarte Vicandi (puriarte) <msc24h18@badile09.ee.ethz.ch>
2024-09-25 09:47:40 +0200

    Add submodule

--- scripts/analyze_pulsedAmplitudeSweep.sh
+++ scripts/analyze_pulsedAmplitudeSweep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SCRIPT_SOURCE="/scratch/msc24h18/MLP_NeuroSim_V3.0/nonlinear_fit.py"
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+PARENT_DIR=$(dirname "$SCRIPT_DIR")
+ANALYSIS_SCRIPT_SOURCE="${PARENT_DIR}/MLP_NeuroSim_V3.0/nonlinear_fit.py"
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <directory>"
@@ -10,11 +12,11 @@ fi
 directory="$1"
 echo "Analyzing data in ${directory}"
 
-# Find all files in the directory that contain the string "pulsedAmplitudeSweep"
+# Find all csv files in the directory that contain the string "pulsedAmplitudeSweep" and don't contain the string "(ALL)"
 files=$(find ${directory} -type f -name "*pulsedAmplitudeSweep*.csv" | grep -v "(ALL)")
 for file in $files; do
     echo "$file"
-    python ${SCRIPT_SOURCE} $file saveplot > /dev/null
+    #python ${ANALYSIS_SCRIPT_SOURCE} $file saveplot > /dev/null
 done
 
 echo "Done."
