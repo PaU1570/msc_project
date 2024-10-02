31faee655141eb0f47e12d36cb270d2be78eb70d
msc24h18 Paul Uriarte Vicandi (puriarte) <msc24h18@badile09.ee.ethz.ch>
2024-10-02 09:55:53 +0200

    Move plot_summary to plot_utils

--- utils/src/plot_utils.py
+++ utils/src/plot_utils.py
@@ -1,4 +1,12 @@
 import argparse
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as clr
+from matplotlib import colormaps
+import seaborn as sns
 
 def create_parser():
     """
@@ -31,9 +39,11 @@ def create_parser():
     parser_summary.add_argument('-y', type=str, help='Y axis column')
     parser_summary.add_argument('-x', type=str, help='X axis column')
     parser_summary.add_argument('--all', action='store_true', help='Plot all pairs of columns in a seaborn pairplot')
+    parser_summary.set_defaults(func=plot_summary)
 
     parser_epochs = subparsers.add_parser('epochs', help='Plot training evolution over epochs', parents=[common_parser])
     parser_epochs.add_argument('-y', type=str, help='Y axis value', choices=['accuracy', 'read_latency', 'write_latency', 'read_energy', 'write_energy'], default='accuracy')
+    parser_epochs.set_defaults(func=plot_epochs)
 
     args = parser.parse_args()
 
@@ -45,6 +55,86 @@ def create_parser():
             parser.error("Must specify both -x and -y")
         if not (args.x or args.y) and not args.all:
             parser.error("Must specify either -x and -y or --all")
+    elif args.subcommand == 'epochs':
+        pass # no checks yet
+    else:
+        parser.error("Please specify a subcommand")
 
     return args
 
+
+def plot_summary(args):
+
+    if not os.path.isfile(args.input):
+        print(f"Error: The file {args.input} does not exist.")
+        exit(1)
+
+    xscale = 'linear' if args.scale in ['linear', 'log-lin'] else 'log'
+    yscale = 'linear' if args.scale in ['linear', 'lin-log'] else 'log'
+
+    title = args.title if args.title is not None else f'{args.input}'
+
+    data = pd.read_csv(args.input)
+    if args.filter is not None:
+        if args.filter[0] not in data.columns:
+            print(f"Error: {args.filter[0]} is not a column in the data.")
+            exit(1)
+        print(f'Filtering data based on {args.filter[0]} = {args.filter[1]}')
+        try:
+            filter_value = pd.to_numeric(args.filter[1])
+        except ValueError:
+            filter_value = args.filter[1]
+
+        data = data[data[args.filter[0]] == filter_value]
+        if data.empty:
+            print(f"Error: No data found for {args.filter[0]} = {args.filter[1]}.")
+            exit(1)
+        data = data.reset_index(drop=True)
+
+    if not args.all:
+        if args.x not in data.columns :
+            print(f"Error: {args.x} is not a column in the data.")
+            exit(1)
+        if args.y not in data.columns :
+            print(f"Error: {args.y} is not a column in the data.")
+            exit(1)
+
+        fig, ax = plt.subplots(figsize=(8, 6))
+
+        unique_types = data[args.hue].unique()
+        
+        if data[args.hue].dtype == 'float64':
+            norm = clr.LogNorm() if args.huescale == 'log' else clr.Normalize()       
+            scatter = ax.scatter(data[args.x], data[args.y], c=data[args.hue], cmap='plasma', norm=norm)
+            cbar = plt.colorbar(scatter)
+            cbar.set_label(args.hue)
+        else:
+            #colors = sns.color_palette('husl', len(unique_types))
+            colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_types)))
+            for unique_type, color in zip(unique_types, colors):
+                type_data = data[data[args.hue] == unique_type]
+                ax.scatter(type_data[args.x], type_data[args.y], label=unique_type, color=color)
+            ax.legend(title=args.hue)
+
+        ax.set(xlabel=args.x, ylabel=args.y, xscale=xscale, yscale=yscale, title=title, aspect=args.aspect, xlim=args.xlim, ylim=args.ylim)
+        if args.savefig is not None:
+            plt.tight_layout()
+            plt.savefig(args.savefig, facecolor=fig.get_facecolor())
+        plt.show()
+
+    if args.all:
+        cols = ['stepSize','pulseWidth','onOffRatio','accuracy', 'A_LTP', 'A_LTD']
+        g = sns.pairplot(data, hue=args.hue, vars=cols)
+        g.fig.suptitle(title)
+        if args.savefig is not None:
+            plt.tight_layout()
+            plt.savefig(args.savefig)
+        plt.show()
+
+def plot_epochs(args):
+    pass
+
+if __name__ == '__main__':
+    plt.style.use('ggplot')
+    args = create_parser()
+    args.func(args)
