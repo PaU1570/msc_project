431657513b6914ff1b00394bd82a3b3e541c71ad
PaU1570 <pauluriarte2@gmail.com>
2024-10-29 09:54:51 +0100

    Update script

--- scripts/aihwkit/run_mnist_on_analyzed.sh
+++ scripts/aihwkit/run_mnist_on_analyzed.sh
@@ -4,13 +4,22 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 PARENT_DIR=$(dirname "$SCRIPT_DIR")
 ANALYSIS_SCRIPT_SOURCE="${SCRIPT_DIR}/mnist_simple.py"
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <analyzed data directory> <output directory>"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <analyzed data directory> <output directory> [epochs (25)] [dw_min_dtod (0.3)] [dw_min_std (0.3)]"
     exit 1
 fi
 
 data_directory="$1"
 output_directory="${2}/aihwkit"
+epochs="${3:-25}"
+dw_min_dtod="${4:-0.3}"
+dw_min_std="${5:-0.3}"
+
+echo "data_directory: $data_directory"
+echo "output_directory: $output_directory"
+echo "epochs: $epochs"
+echo "dw_min_dtod: $dw_min_dtod"
+echo "dw_min_std: $dw_min_std"
 
 if [ ! -d "$output_directory" ]; then
     mkdir -p "$output_directory"
@@ -21,7 +30,7 @@ files=$(find ${data_directory} -type f -name "*_Summary.dat")
 for file in $files; do
     echo -e "\e[33m$file\e[0m"
 
-    if ! python ${ANALYSIS_SCRIPT_SOURCE} $file --output_dir ${output_directory} --epochs 25 --early_stopping --dw_min_dtod 0.5 --dw_min_std 1.0 ; then
+    if ! python ${ANALYSIS_SCRIPT_SOURCE} $file --output_dir ${output_directory} --epochs ${epochs} --dw_min_dtod ${dw_min_dtod} --dw_min_std ${dw_min_std} ; then
         echo -e "\e[31mError\e[0m analyzing file"
     else
         echo -e "\e[32mDone\e[0m"
