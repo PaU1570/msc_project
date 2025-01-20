a2234df6f87daa1c126a95ccca507312aef83951
PaU1570 <pauluriarte2@gmail.com>
2025-01-20 17:12:11 +0100

    Add lorentzian fit

--- src/msc_project/utils/fit_NLS.py
+++ src/msc_project/utils/fit_NLS.py
@@ -2,11 +2,20 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 from scipy.optimize import curve_fit
+from scipy.integrate import quad
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as clr
 import argparse
 import os
+
+def r2_score(y, y_pred):
+    """
+    Calculate the R^2 score.
+    """
+    ss_res = np.sum((y - y_pred)**2)
+    ss_tot = np.sum((y - np.mean(y))**2)
+    return 1 - ss_res / ss_tot
 
 def distribution_function_mesa(z, Gamma, z1, z2):
     """
@@ -24,14 +33,15 @@ def distribution_function_mesa(z, Gamma, z1, z2):
         
     return np.vectorize(_distribution_function_mesa)(z, Gamma, z1, z2)
     
-def distribution_function_lorentzian(z, A, omega, tl):
+def distribution_function_lorentzian(z, A, omega, z_lorentz):
     """
     Distribution function g(z) with a Lorentzian shape, as used in Kondratyuk & Chouprik (2022).
     """
-    def _distribution_function_lorentzian(z, A, omega, tl):
-        return (A / np.pi) * (omega / ((z - np.log10(tl))**2 + omega**2))
+    def _distribution_function_lorentzian(z, A, omega, z_lorentz):
+        return (A / np.pi) * (omega / ((z - z_lorentz)**2 + omega**2))
     
-    return np.vectorize(_distribution_function_lorentzian)(z, A, omega, tl)
+    #return np.vectorize(_distribution_function_lorentzian)(z, A, omega, z_lorentz)
+    return _distribution_function_lorentzian(z, A, omega, z_lorentz)
 
 def polarization_mesadist(t, Gamma, z1, z2):
     """
@@ -49,17 +59,19 @@ def polarization_mesadist(t, Gamma, z1, z2):
         
     return np.vectorize(_polarization_mesadist)(t, Gamma, z1, z2)
     
-def polarization_lorentzian(t, A, omega, tl):
+def polarization_lorentzian(t, A, omega, z_lorentz, n):
     """
     Polarization function P(t) arising from a Lorentzian distribution function g(z) as used in Kondratyuk & Chouprik (2022).
     """
-    def _polarization_lorentzian(t, A, omega, tl):
-        z0 = np.log10(t)
-        return (A/np.pi) * np.arctan((z0 - np.log10(tl)) / omega)
+    def _integrand(z0, t, A, omega, zl, n):
+        t0 = 10**z0
+        return (1 - np.exp(-(t/t0)**n)) * distribution_function_lorentzian(z0, A, omega, zl)
+    def _polarization_lorentzian(t, A, omega, zl, n):
+        return 2 * quad(_integrand, zl-10*omega, zl+10*omega, args=(t, A, omega, zl, n), limit=100000)[0]
     
-    return np.vectorize(_polarization_lorentzian)(t, A, omega, tl)
+    return np.vectorize(_polarization_lorentzian)(t, A, omega, z_lorentz, n)
 
-def fit_polarization(x, y, p0=None, type="mesa", pmax=None):
+def fit_polarization(x, y, p0=None, bounds=None, type="mesa", pmax=None):
     """
     Fit the polarization data to a distribution function.
     """
@@ -77,8 +89,10 @@ def fit_polarization(x, y, p0=None, type="mesa", pmax=None):
         else:
             raise ValueError("Unknown type of distribution function")
         
+    if bounds is None:
+        bounds = (-np.inf, np.inf)
     
-    popt, pcov = curve_fit(_func, x, y, p0=p0, nan_policy='omit')
+    popt, pcov = curve_fit(_func, x, y, p0=p0, bounds=bounds, nan_policy='omit')
     return popt
 
 def f_tau(V, tau0, V0, n):
@@ -92,9 +106,13 @@ def fit_tau(voltage, log_tau, p0=None):
     log_tau = np.array(log_tau)
 
     if p0 is None:
-        p0 = [1e-6, 10, 2]
+        p0 = [1, 1, 1]
+        pass
+
+    bounds = ([-np.inf, 0, 0], [np.inf, np.inf, 4])
+    #bounds = (-np.inf, np.inf)
     
-    popt, pcov = curve_fit(lambda V, t0, V0, n: np.log(f_tau(V, t0, V0, n)), voltage, log_tau, p0=p0, nan_policy='omit')
+    popt, pcov = curve_fit(lambda V, t0, V0, n: np.log(f_tau(V, t0, V0, n)), voltage, log_tau, p0=p0, bounds=bounds, nan_policy='omit')
     return popt
 
 def iterative_fit(data, pmax=None, iters=10, p0_tmin=None, p0_tmax=None):
@@ -106,6 +124,8 @@ def iterative_fit(data, pmax=None, iters=10, p0_tmin=None, p0_tmax=None):
     optdata = {}
     popt_tmin = None
     popt_tmax = None
+
+    bounds = ([1e-6, -12, -12], [10, 0, 0])
 
     for i in range(iters):
         optdata = {"voltage": [], "p0": [], "p1": [], "p2": []}
@@ -122,7 +142,7 @@ def iterative_fit(data, pmax=None, iters=10, p0_tmin=None, p0_tmax=None):
                 tmin0 = np.log(f_tau(float(col), *popt_tmin))
                 p0 = [0.2, tmin0, tmin0+1]
             try:
-                popt = fit_polarization(data['Pulse Width'], data[col], p0=p0, pmax=pmax[float(col)] if pmax is not None else None)
+                popt = fit_polarization(data['Pulse Width'], data[col], p0=p0, bounds=bounds, pmax=pmax[float(col)] if pmax is not None else None)
                 # keep fit only if it looks reasonable
                 if (popt is not None) and (popt[1] < popt[2]) and (popt[1] < 10) and (popt[2] < 10) and (popt[1] > -12) and (popt[2] > -12):
                     optdata["voltage"].append(float(col))
@@ -144,30 +164,87 @@ def iterative_fit(data, pmax=None, iters=10, p0_tmin=None, p0_tmax=None):
 
     return pd.DataFrame(optdata), popt_tmin, popt_tmax
 
+def iterative_fit_lorentzian(data, pmax=None, iters=5, p0_lorentz=None, exclude_voltages=None):
+    """
+    Fit polarization data (lorentzian), extract tau_lorentz fit, and then use that to obtain better initial guesses for polarization fit.
+    """
+    # initial guess
+    p0 = [0.2, 0.1, -7, 2]
+    optdata = {}
+    popt_tl = None                    
 
-def plot_single(data, column, ax=None, fit=False, fit_type="mesa", p0=None, pmax=None, **kwargs):
+    bounds = ([0, 1e-11, -10, 0], [10, 0.9, 0, 3])
+    iters = 1
+    for i in range(iters):
+        optdata = {"voltage": [], "p0": [], "p1": [], "p2": [], "p3": []}
+        for col in data.columns[1:]:
+            # update initial guess
+            # if (popt_tl is not None):
+            #     tl0 = np.log(f_tau(float(col), *popt_tl))
+            #     p0 = [0.2, 0.1, tl0, popt_tl[2]]
+            try:
+                popt = fit_polarization(data['Pulse Width'], data[col], type="lorentzian", p0=p0, bounds=bounds, pmax=pmax[float(col)] if pmax is not None else None)
+                # keep fit only if it looks reasonable
+                if (popt is not None) and (popt[2] < 10) and (popt[2] > -12):
+                    optdata["voltage"].append(float(col))
+                    optdata["p0"].append(popt[0])
+                    optdata["p1"].append(popt[1])
+                    optdata["p2"].append(popt[2])
+                    optdata["p3"].append(popt[3])
+
+                print(f"Fit for {col}V: {popt}")
+            except Exception as e:
+                print("Failed to fit", col, e)
+
+        # fit tau_lorentz
+        try:
+            x = np.array(optdata["voltage"])
+            y = np.array(optdata["p2"])
+            if exclude_voltages is not None:
+                mask = np.isin(x, exclude_voltages, invert=True)
+                x = x[mask]
+                y = y[mask]
+            popt_tl = fit_tau(x, y, p0=p0_lorentz)
+        except Exception as e:
+            print("Failed to fit tau_lorentz", e)
+
+    return pd.DataFrame(optdata), popt_tl
+
+
+def plot_single(data, column, ax=None, fit=False, fit_type="mesa", p0=None, bounds=None, pmax=None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
 
     popt = None
     if fit:
         ax.scatter(data['Pulse Width'], data[column], **kwargs)
-        try:
-            if p0 is None:
-                p0 = [0.2, np.log(f_tau(float(column), 1e-6, 4, 2)), np.log(f_tau(float(column), 1e-4, 4, 2))]
-            popt = fit_polarization(data['Pulse Width'], data[column], p0=p0, type=fit_type, pmax=pmax)
-            t = np.logspace(np.log10(data['Pulse Width'].min()), np.log10(data['Pulse Width'].max()), 1000)
+        if p0 is None:
             if fit_type == "mesa":
-                p = polarization_mesadist(t, *popt) * (pmax if pmax is not None else 1)
-                label = f'{column}V: $\\Gamma$={popt[0]:.2f}, $\\log(\\tau_{{min}})$={popt[1]:.2f}, $\\log(\\tau_{{max}})$={popt[2]:.2f}'
-            elif fit_type == "lorentzian":
-                p = polarization_lorentzian(t, *popt) * (pmax if pmax is not None else 1)
-                label = f'{column}V: $A$={popt[0]:.2f}, $\\omega$={popt[1]:.2f}, $\\log(\\tau_{{Lorentz}})$={popt[2]:.2f}'
+                p0 = [0.2, np.log(f_tau(float(column), 1e-6, 4, 2)), np.log(f_tau(float(column), 1e-4, 4, 2))]
             else:
-                raise ValueError("Unknown type of distribution function")
-            ax.plot(t, p, ls='--', label=label, **kwargs)
+                p0 = [0.2, 0.1, -7, 2]
+        if bounds is None:
+            if fit_type == "mesa":
+                bounds = ([1e-6, -12, -12], [10, 0, 0])
+            else:
+                bounds = ([0, 1e-11, -10, 0], [10, 0.9, 0, 3])
+        try:
+            popt = fit_polarization(data['Pulse Width'], data[column], p0=p0, bounds=bounds, type=fit_type, pmax=pmax)
         except Exception as e:
-            print(f"Failed to fit {column}V", e)
+            print(f"Failed to fit {column}V: {e}")
+            return
+        t = np.logspace(np.log10(data['Pulse Width'].min()), np.log10(data['Pulse Width'].max()), 1000)
+        if fit_type == "mesa":
+            p = polarization_mesadist(t, *popt) * (pmax if pmax is not None else 1)
+            r2 = r2_score(data[column], polarization_mesadist(data['Pulse Width'], *popt))
+            label = f'{column}V: $\\Gamma$={popt[0]:.2f}, $\\log(\\tau_{{min}})$={popt[1]:.2f}, $\\log(\\tau_{{max}})$={popt[2]:.2f} ($R^2$={r2:.3f})'
+        elif fit_type == "lorentzian":
+            p = polarization_lorentzian(t, *popt) * (pmax if pmax is not None else 1)
+            r2 = r2_score(data[column], polarization_lorentzian(data['Pulse Width'], *popt))
+            label = f'{column}V: $A$={popt[0]:.2f}, $\\omega$={popt[1]:.2f}, $\\log(\\tau_{{Lorentz}})$={popt[2]:.2f}, $n$={popt[3]:.2f}, ($R^2$={r2:.3f})'
+        else:
+            raise ValueError("Unknown type of distribution function")
+        ax.plot(t, p, ls='--', label=label, **kwargs)
     else:
         ax.plot(data['Pulse Width'], data[column], marker='o', **kwargs)
 
@@ -203,8 +280,11 @@ def plot_all(data, ax=None, cmap=plt.cm.plasma, fit=False, fit_type="mesa", pmax
     
     return pd.DataFrame(optdata)
 
-def plot_all_iterative(data, axs, pmax=None, cmap=plt.cm.plasma, iters=10, p0_tmin=None, p0_tmax=None):
-    opt_df, popt_tmin, popt_tmax = iterative_fit(data, pmax=pmax, iters=iters, p0_tmin=p0_tmin, p0_tmax=p0_tmax)
+def plot_all_iterative(data, axs, pmax=None, cmap=plt.cm.plasma, iters=5, p0_tmin=None, p0_tmax=None, fit_type="mesa", exclude_voltages=None):
+    if fit_type == "mesa":
+        opt_df, popt_tmin, popt_tmax = iterative_fit(data, pmax=pmax, iters=iters, p0_tmin=p0_tmin, p0_tmax=p0_tmax)
+    else:
+        opt_df, popt_tl = iterative_fit_lorentzian(data, pmax=pmax, iters=iters, p0_lorentz=p0_tmin, exclude_voltages=exclude_voltages)
 
     norm = clr.Normalize()       
     colors = cmap(norm(data.columns[1:].astype(float)))
@@ -215,24 +295,38 @@ def plot_all_iterative(data, axs, pmax=None, cmap=plt.cm.plasma, iters=10, p0_tm
         axs[0].scatter(data['Pulse Width'], data[col], color=c)
         if float(col) in opt_df['voltage'].values:
             popt = opt_df[opt_df['voltage'] == float(col)]
-            gamma, z1, z2 = popt['p0'].values[0], popt['p1'].values[0], popt['p2'].values[0]
             t = np.logspace(np.log10(data['Pulse Width'].min()), np.log10(data['Pulse Width'].max()), 1000)
-            label = f'{col}V: $\\Gamma$={gamma:.2f}, $\\log(\\tau_{{min}})$={z1:.2f}, $\\log(\\tau_{{max}})$={z2:.2f}'
-            p = polarization_mesadist(t, gamma, z1, z2) * (pmax[float(col)] if pmax is not None else 1)
+            if fit_type == "mesa":
+                gamma, z1, z2 = popt['p0'].values[0], popt['p1'].values[0], popt['p2'].values[0]
+                label = f'{col}V: $\\Gamma$={gamma:.2f}, $\\log(\\tau_{{min}})$={z1:.2f}, $\\log(\\tau_{{max}})$={z2:.2f}'
+                p = polarization_mesadist(t, gamma, z1, z2) * (pmax[float(col)] if pmax is not None else 1)
+            else:
+                A, omega, zl, n = popt['p0'].values[0], popt['p1'].values[0], popt['p2'].values[0], popt['p3'].values[0]
+                label = f'{col}V: $A$={A:.2f}, $\\omega$={omega:.2f}, $\\log(\\tau_{{Lorentz}})$={zl:.2f}, $n$={n:.2f}'
+                p = polarization_lorentzian(t, A, omega, zl, n) * (pmax[float(col)] if pmax is not None else 1)
             axs[0].plot(t, p, ls='--', color=c, label=label)
 
-    axs[1].scatter(opt_df['voltage'], opt_df['p1'], color='C0', label='$\\log(\\tau_{{min}})$ (exp.)')
-    axs[1].scatter(opt_df['voltage'], opt_df['p2'], color='C1', label='$\\log(\\tau_{{max}})$ (exp.)')
-
     v = np.linspace(opt_df['voltage'].min(), opt_df['voltage'].max(), 1000)
-    if popt_tmin is not None:
-        tmin = f_tau(v, *popt_tmin)
-        axs[1].plot(v, np.log(tmin), ls='--', color='C0',
-                    label=f'$\\log(\\tau_{{min}})$ (fit): $\\tau_0$={popt_tmin[0]:.2e}, $V_0$={popt_tmin[1]:.2f}, $n$={popt_tmin[2]:.2f}')
-    if popt_tmax is not None:
-        tmax = f_tau(v, *popt_tmax)
-        axs[1].plot(v, np.log(tmax), ls='--', color='C1',
-                    label=f'$\\log(\\tau_{{max}})$ (fit): $\\tau_0$={popt_tmax[0]:.2e}, $V_0$={popt_tmax[1]:.2f}, $n$={popt_tmax[2]:.2f}')
+
+    if fit_type == "mesa":
+        axs[1].scatter(opt_df['voltage'], opt_df['p1'], color='C0', label='$\\log(\\tau_{{min}})$ (exp.)')
+        axs[1].scatter(opt_df['voltage'], opt_df['p2'], color='C1', label='$\\log(\\tau_{{max}})$ (exp.)')
+
+        if popt_tmin is not None:
+            tmin = f_tau(v, *popt_tmin)
+            axs[1].plot(v, np.log(tmin), ls='--', color='C0',
+                        label=f'$\\log(\\tau_{{min}})$ (fit): $\\tau_0$={popt_tmin[0]:.2e}, $V_0$={popt_tmin[1]:.2f}, $n$={popt_tmin[2]:.2f}')
+        if popt_tmax is not None:
+            tmax = f_tau(v, *popt_tmax)
+            axs[1].plot(v, np.log(tmax), ls='--', color='C1',
+                        label=f'$\\log(\\tau_{{max}})$ (fit): $\\tau_0$={popt_tmax[0]:.2e}, $V_0$={popt_tmax[1]:.2f}, $n$={popt_tmax[2]:.2f}')
+            
+    else:
+        axs[1].scatter(opt_df['voltage'], opt_df['p2'], color='C0', label='$\\log(\\tau_{{Lorentz}})$ (exp.)')
+        if popt_tl is not None:
+            tl = f_tau(v, *popt_tl)
+            axs[1].plot(v, np.log(tl), ls='--', color='C0',
+                        label=f'$\\log(\\tau_{{Lorentz}})$ (fit): $\\tau_0$={popt_tl[0]:.2e}, $V_0$={popt_tl[1]:.2f}, $n$={popt_tl[2]:.2f}')
 
 def load_data(filename):
     """
@@ -265,31 +359,43 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='Input file')
     parser.add_argument('--voltage', type=float, help='Voltage to fit')
-    parser.add_argument('--p0', type=float, nargs=3, help='Initial guess for the fit parameters')
+    parser.add_argument('--p0', type=float, nargs='+', help='Initial guess for the fit parameters')
+    parser.add_argument('--bounds_lower', type=float, nargs='+', help='Lower bounds for the fit parameters')
+    parser.add_argument('--bounds_upper', type=float, nargs='+', help='Upper bounds for the fit parameters')
     parser.add_argument('--p0_tmin', type=float, nargs=3, help='Initial guess for the fit parameters of tmin')
     parser.add_argument('--p0_tmax', type=float, nargs=3, help='Initial guess for the fit parameters of tmax')
     parser.add_argument('--fit_type', type=str, choices=['mesa', 'lorentzian'], default='mesa', help='Type of distribution function to fit')
     parser.add_argument('--pmax', type=str, help='File containing maximum polarization values for each voltage')
+    parser.add_argument('--exclude_voltages', type=float, nargs='+', help='Voltages to exclude from the fit')
 
     args = parser.parse_args()
 
     data = load_data(args.input)
     if args.pmax:
         pmax = np.loadtxt(args.pmax, delimiter=',', skiprows=1)
-        pmax = {float(v): min(p*1, 1) for v, p in pmax}
+        pmax = {float(v): min(p*1.05, 1) for v, p in pmax}
     else:
-        pmax = {float(v): 1.05 for v in data.columns[1:]}
+        # default to max polarization of 1 for every voltage if no pmax file is provided
+        pmax = {float(v): 1 for v in data.columns[1:]}
+
+    bounds = None
+    if args.bounds_lower and args.bounds_upper:
+        bounds = (args.bounds_lower, args.bounds_upper)
+    elif args.bounds_lower:
+        bounds = (args.bounds_lower, np.inf)
+    elif args.bounds_upper:
+        bounds = (-np.inf, args.bounds_upper)
 
     if args.voltage:
         fig, ax = plt.subplots()
-        plot_single(data, f'{args.voltage:.2f}', ax=ax, fit=True, fit_type=args.fit_type, p0=args.p0, pmax=pmax[args.voltage])
+        plot_single(data, f'{args.voltage:.2f}', ax=ax, fit=True, fit_type=args.fit_type, p0=args.p0, bounds=bounds, pmax=pmax[args.voltage])
         ax.set(xlabel = 'Pulse duration', ylabel = 'Partial polarization', xscale='log', ylim=(0, 1))
         ax.legend()
         plt.show()
     else:
         fig, axs = plt.subplots(1, 2, figsize=(20, 10))
         fig.suptitle(args.input)
-        plot_all_iterative(data, axs, pmax=pmax, p0_tmin=args.p0_tmin, p0_tmax=args.p0_tmax)
+        plot_all_iterative(data, axs, pmax=pmax, p0_tmin=args.p0_tmin, p0_tmax=args.p0_tmax, fit_type=args.fit_type, exclude_voltages=args.exclude_voltages)
         axs[0].set(xlabel = 'Pulse duration', ylabel = 'Partial polarization', xscale='log', ylim=(0, 1))
         axs[0].legend()
         axs[1].set(xlabel = 'V', ylabel = '$\\log(\\tau)$')
